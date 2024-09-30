a7c878df6c44c02c94c56ff39761126e50e275cc
oscar <oherrerogordaliza21@gmail.com>
2024-10-01 00:53:47 +0200

    train data loaded

--- animals.py
+++ animals.py
@@ -9,11 +9,16 @@ import cv2
 import csv
 
 class Animals_clisfier:
-    def __init__(self,f_name):
-        with open(f_name,'r') as f:
+    def __init__(self,f_train,f_test):
+        with open(f_train,'r') as f:
             csv_r = csv.reader(f)
-            paths = [i for i in csv_r if len(i) > 0]
+            trn_r = [i for i in csv_r if len(i) > 0]
+        self._trainX, self._trainY = [cv2.imread(i[0]) for i in trn_r],[i[1] for i in trn_r]
+        with open(f_test,'r') as f:
+            tst_r = f.readlines()
+        self._trainY = [cv2.imread(i[0]) for i in tst_r]
+        print(self._trainY[0])
 
-a_cls = Animals_clisfier("train.csv")
+a_cls = Animals_clisfier("train.csv","test.txt")
 
 
