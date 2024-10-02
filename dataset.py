93e789b2d90412e463c305c4bb563bfc65941596
oscar <oherrerogordaliza21@gmail.com>
2024-10-02 13:45:15 +0200

    images content saved on the csv

--- dataset.py
+++ dataset.py
@@ -1,13 +1,18 @@
 import os 
 import pandas as pd
+from PIL import Image
+import numpy as np
 
 def take_images(directory ='.'):
-    paths = []
+    paths,images = [],[]
     for root,_,files in os.walk(directory):
         for file in files:
             completed_path = os.path.join(root,file)
             paths.append(completed_path)
-    return paths
+            img = Image.open(completed_path)
+            img = img.resize((128,128))
+            images.append(np.array(img))
+    return paths,images
 
 def take_tags(paths):
     tags = []
@@ -24,9 +29,9 @@ def save_test(elements:list,name:str):
     with open(name,'w') as f:
         f.writelines(elements)
 
-path_train = take_images('.\\mg-animal-prediction-24-25\\train_images')
-path_test = take_images('.\\mg-animal-prediction-24-25\\test_images')
+path_train,images = take_images('.\\mg-animal-prediction-24-25\\train_images')
+path_test,_ = take_images('.\\mg-animal-prediction-24-25\\test_images')
 tags = take_tags(path_train)
 
-save_dataset(path_train,tags)
+save_dataset(images,tags)
 save_test(path_test,"test.txt")
