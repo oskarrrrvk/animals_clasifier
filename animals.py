ca77dfa108b7f455e2a518573b6dcb2228b6a232
oscar <oherrerogordaliza21@gmail.com>
2024-10-03 01:10:46 +0200

    animals.py changed

--- animals.py
+++ animals.py
@@ -15,24 +15,23 @@ class Animals_clisfier:
     def __init__(self,f_train):
         f = pd.read_csv(f_train)
 
-        x = f.drop('tags',axis=1)
-        y = f['tags']
+        x = f.drop('encode_tags',axis=1)
+        y = f['encode_tags']
 
         train_X,test_X,train_Y,test_Y = train_test_split(x['image'],y,test_size=0.2,random_state=42)
-        self._train_Y = [train_Y.iloc[i] for i in range(len(train_Y))]
-        self._test_Y = [test_Y.iloc[i] for i in range(len(test_Y))]
+        self._train_Y = np.array([train_Y.iloc[i] for i in range(len(train_Y))])
+        self._test_Y = np.array([test_Y.iloc[i] for i in range(len(test_Y))])
         self._train_X = self._open_img(train_X)
         self._test_X = self._open_img(test_X)
-        print(f"train length: {len(self._train_X),len(self._train_Y)}")
-        print(f"test lenght: {len(self._test_X),len(self._test_Y)}")
+        print(f"train length: {self._train_X.shape,self._train_Y.shape}")
+        print(f"test lenght: {self._test_X.shape,self._test_Y.shape}")
 
     def _open_img(self,x):
         result = []
         for i in range(len(x)):
             img = Image.open(x.iloc[i])
-            img = img.resize((128,128))
             result.append(np.array(img))
-        return result
+        return np.array(result)
 
     def create_model(self):
         input = Input(shape=(128,128,1))
@@ -43,8 +42,8 @@ class Animals_clisfier:
         self._model = Model(input,output)
     
     def execute_model(self):
-        self._model.compile()
-        self._history = self._model.fit()
+        self._model.compile(loss='categorical_Crossentropy',optimizer='Adam',metrics=['accuracy'])
+        self._history = self._model.fit(self._train_X,self._train_Y,validation_split=0.2,batch_size=256,epochs=50,validation_data=(self._test_X,self._test_Y))
     
     def show_plot_losses(self):
         plt.rcParams['figure.figsize'] = [20, 5]
@@ -72,3 +71,6 @@ class Animals_clisfier:
         self._model.save(name)
 
 a_cls = Animals_clisfier("dataset.csv")
+a_cls.create_model()
+a_cls.execute_model()
+a_cls.show_plot_losses()
