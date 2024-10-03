837a36f793a8f9c5dde0288e14ed3e88289fb85e
oscar <oherrerogordaliza21@gmail.com>
2024-10-03 20:08:29 +0200

    neural network can read the images

--- animals.py
+++ animals.py
@@ -1,49 +1,66 @@
 from matplotlib import pyplot as plt
 
+from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.layers import Input, Dense, Flatten, Conv2D, MaxPooling2D, Dropout
 from tensorflow.keras.utils import plot_model
-from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.models import Model
 
 from PIL import Image
-import numpy as np
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
 class Animals_clisfier:
     def __init__(self,f_train):
+        
         f = pd.read_csv(f_train)
 
         x = f.drop('encode_tags',axis=1)
         y = f['encode_tags']
 
         train_X,test_X,train_Y,test_Y = train_test_split(x['image'],y,test_size=0.2,random_state=42)
-        self._train_Y = np.array([train_Y.iloc[i] for i in range(len(train_Y))])
-        self._test_Y = np.array([test_Y.iloc[i] for i in range(len(test_Y))])
+        
+        self._train_Y = to_categorical(np.array([train_Y.iloc[i] for i in range(len(train_Y))]))
+        self._test_Y = to_categorical(np.array([test_Y.iloc[i] for i in range(len(test_Y))]))
+        
         self._train_X = self._open_img(train_X)
         self._test_X = self._open_img(test_X)
-        print(f"train length: {self._train_X.shape,self._train_Y.shape}")
-        print(f"test lenght: {self._test_X.shape,self._test_Y.shape}")
+        
+        print(f"train length: {len(self._train_X),self._train_Y.shape}")
+        print(f"test lenght: {len(self._test_X),self._test_Y.shape}")
+        
 
     def _open_img(self,x):
         result = []
         for i in range(len(x)):
             img = Image.open(x.iloc[i])
-            result.append(np.array(img))
+            img = img.resize((128,128))
+            if img.mode != 'RGB':
+                img = img.convert('RGB')
+            img = np.array(img)
+            img= img.astype(np.float16)
+            img = img/255.0
+            if img.ndim == 2:
+                img = np.expand_dims(img,axis=-1)
+            result.append(img)
         return np.array(result)
 
     def create_model(self):
-        input = Input(shape=(128,128,1))
+        input = Input(shape=(128,128,3))
         x = Conv2D(16,(3,3),activation='ReLU')(input)
         x = MaxPooling2D((2,2))(x)
+        x = Conv2D(32,(3,3),activation='ReLU')(x)
+        x = MaxPooling2D((2,2))(x)
+        x = Conv2D(64,(3,3),activation='ReLU')(x)
+        x = MaxPooling2D((2,2))(x)
         x = Flatten()(x)
+        x = Dense(8,activation='softmax')(x)
         output = Dense(10,activation='softmax')(x)
         self._model = Model(input,output)
     
     def execute_model(self):
-        self._model.compile(loss='categorical_Crossentropy',optimizer='Adam',metrics=['accuracy'])
-        self._history = self._model.fit(self._train_X,self._train_Y,validation_split=0.2,batch_size=256,epochs=50,validation_data=(self._test_X,self._test_Y))
+        self._model.compile(loss='categorical_crossentropy',optimizer='Adam',metrics=['accuracy'])
+        self._history = self._model.fit(self._train_X,self._train_Y,validation_split=0.2,batch_size=64,epochs=50,validation_data=(self._test_X,self._test_Y))
     
     def show_plot_losses(self):
         plt.rcParams['figure.figsize'] = [20, 5]
